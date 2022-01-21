efbbd969cf257b143cd00b390e25e24f30f3e6a8
Vincent Brown <admin@gmasterplanner.com>
2022-01-20 20:23:25 -0500

    update domain models project

---
+++ Busidex.DomainModels/SystemSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace Busidex.DomainModels
+{
+    public class SystemSetting
+    {
+        public int SystemSettingId { get; set; }
+        public string Setting { get; set; }
+        public string Value { get; set; }
+    }
+}
